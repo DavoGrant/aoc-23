244835cb79031c7844a1eacb8a5404c0942f5486
David Grant <davidttgg@gmail.com>
2023-12-03 18:49:40 +0000

    Game ids sum rd 1.

--- day_02/day_02.cpp
+++ day_02/day_02.cpp
@@ -2,6 +2,28 @@
 #include <fstream>
 
 
+struct CubeDraw
+{
+    int number;
+    std::string colour;
+};
+
+
+CubeDraw getCubeDrawNumberAndColour(const std::string& drawLabel) {
+
+    CubeDraw cubes;
+    for (std::string::size_type i = 0; i < drawLabel.size(); i++) {
+        if (drawLabel[i] == ' ') {
+            cubes.number = std::stoi(drawLabel.substr(0, i));
+            cubes.colour = drawLabel.substr(i + 1, drawLabel.size() - i + 1);
+            break;
+        }
+    }
+
+    return cubes;
+}
+
+
 int sumOfGameIDs(const std::string& file_name) {
 
     std::string line;
@@ -12,13 +34,34 @@ int sumOfGameIDs(const std::string& file_name) {
         while (getline(f, line)) {
 
             int gameID = 0;
+            bool gamePossible = true;
+            int lastSpaceIdx = 0;
             for (std::string::size_type i = 0; i < line.size(); i++) {
 
                 if (line[i] == ':') {
                     gameID = std::stoi(line.substr(5, i + 1));
+                    lastSpaceIdx = i + 1;
+                }
+
+                if (line[i] == ',' || line[i] == ';' || i == line.size() - 1) {
+
+                    int endLineBump = (i == line.size() - 1) ? 1 : 0;
+                    std::string drawLabel = line.substr(lastSpaceIdx + 1, i - lastSpaceIdx - 1 + endLineBump);
+                    CubeDraw cubes = getCubeDrawNumberAndColour(drawLabel);
+                    lastSpaceIdx = i + 1;
+
+                    if (cubes.colour == "red" && cubes.number > 12) {
+                        gamePossible = false;
+                    } if (cubes.colour == "green" && cubes.number > 13) {
+                        gamePossible = false;
+                    } else if (cubes.colour == "blue" && cubes.number > 14) {
+                        gamePossible = false;
+                    }
                 }
             }
-            std::cout << gameID << std::endl;
+
+            if (gamePossible) sum += gameID;
+
         }
     } else {
         perror("Error opening file");
@@ -28,24 +71,8 @@ int sumOfGameIDs(const std::string& file_name) {
 }
 
 
-struct CubeDraw
-{
-    int number;
-    std::string colour;
-};
-
-
-CubeDraw getCubeDrawNumberAndColour(const std::string& line) {
-    CubeDraw cubes;
-    cubes.number = 0;
-    cubes.colour = "blue";
-
-    return cubes;
-}
-
-
 int main() {
-    int result = sumOfGameIDs("data_test_1.txt");
+    int result = sumOfGameIDs("data_real.txt");
     std::cout << "Total game IDs = " << result << std::endl;
     return 0;
 }
