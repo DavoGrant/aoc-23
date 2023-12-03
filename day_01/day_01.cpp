9ff4c621729092381201b1958c7a124fb34da743
David Grant <davidttgg@gmail.com>
2023-12-03 16:44:24 +0000

    Day 1, sum fun.

---
+++ day_01/day_01.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+
+int sumOfCalibrationValues(const std::string& file_name) {
+
+    std::string line;
+    std::ifstream f (file_name);
+
+    int sum = 0;
+    if (f.is_open()) {
+        while (getline(f, line)) {
+
+            int firstNumber = 0;
+            for (std::string::size_type i = 0; i < line.size(); i++) {
+                if (isdigit(line[i]) ){
+                    firstNumber = line[i] - '0';
+                    break;
+                }
+            }
+
+            int lastNumber = 0;
+            for (std::string::size_type i = line.size() - 1; i >=0; i--) {
+                if (isdigit(line[i]) ){
+                    lastNumber = line[i] - '0';
+                    break;
+                }
+            }
+
+            sum += firstNumber * 10 + lastNumber;
+        }
+    } else {
+        perror("Error opening file");
+    }
+
+    return sum;
+}
+
+
+int main() {
+    int result = sumOfCalibrationValues("data_test.txt");
+    std::cout << "Total calibration values = " << result << std::endl;
+    return 0;
+}
---
+++ day_01/run_01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the name of your C++ source file
+cpp_file="day_01.cpp"
+
+# Set the name of the compiled executable
+output_name="day_01.so"
+
+# Compile the C++ program
+g++ -Wall -std=c++17 -o $output_name $cpp_file
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    # Run the compiled program
+    ./$output_name
+else
+    echo "Compilation failed."
+fi
