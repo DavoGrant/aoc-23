73e17d7ae5635e71fc5e821b0d7fec55ae0d279e
David Grant <davidttgg@gmail.com>
2023-12-03 18:58:24 +0000

    Sum of game powers.

--- day_02/day_02.cpp
+++ day_02/day_02.cpp
@@ -71,8 +71,57 @@ int sumOfGameIDs(const std::string& file_name) {
 }
 
 
+int sumOfGamePowers(const std::string& file_name) {
+
+    std::string line;
+    std::ifstream f (file_name);
+
+    int sum = 0;
+    if (f.is_open()) {
+        while (getline(f, line)) {
+
+            int redMin = 0;
+            int greeMin = 0;
+            int blueMin = 0;
+            int lastSpaceIdx = 0;
+            for (std::string::size_type i = 0; i < line.size(); i++) {
+
+                if (line[i] == ':') {
+                    lastSpaceIdx = i + 1;
+                }
+
+                if (line[i] == ',' || line[i] == ';' || i == line.size() - 1) {
+
+                    int endLineBump = (i == line.size() - 1) ? 1 : 0;
+                    std::string drawLabel = line.substr(lastSpaceIdx + 1, i - lastSpaceIdx - 1 + endLineBump);
+                    CubeDraw cubes = getCubeDrawNumberAndColour(drawLabel);
+                    lastSpaceIdx = i + 1;
+
+                    if (cubes.colour == "red" && cubes.number > redMin) {
+                        redMin = cubes.number;
+                    } if (cubes.colour == "green" && cubes.number > greeMin) {
+                        greeMin = cubes.number;
+                    } else if (cubes.colour == "blue" && cubes.number > blueMin) {
+                        blueMin = cubes.number;
+                    }
+                }
+            }
+
+            sum += redMin * greeMin * blueMin;
+
+        }
+    } else {
+        perror("Error opening file");
+    }
+
+    return sum;
+}
+
+
 int main() {
-    int result = sumOfGameIDs("data_real.txt");
-    std::cout << "Total game IDs = " << result << std::endl;
+    int result_1 = sumOfGameIDs("data_real.txt");
+    std::cout << "Total game IDs = " << result_1 << std::endl;
+    int result_2 = sumOfGamePowers("data_real.txt");
+    std::cout << "Total game powers = " << result_2 << std::endl;
     return 0;
 }
