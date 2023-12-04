87b558f7afdaf88b95513856876cd40c085b2857
David Grant <davidttgg@gmail.com>
2023-12-04 18:57:13 +0000

    Recursive soln scratch card counting.

--- day_04/day_04.cpp
+++ day_04/day_04.cpp
@@ -3,6 +3,56 @@
 #include <set>
 
 
+int numMatches(const std::string& line) {
+
+    int nMatches = 0;
+    std::string numberStr = "";
+    bool winningNumbersSegment = false;
+    bool playerNumbersSegment = false;
+    std::set <int> winningNumbers;
+    for (std::string::size_type i = 0; i < line.size(); i++) {
+
+        if (line[i] == ':') {
+            winningNumbersSegment = true;
+        }
+
+        if (winningNumbersSegment) {
+            if (isdigit(line[i])) {
+                numberStr += line[i];
+            } else if (numberStr != "") {
+                winningNumbers.insert(std::stoi(numberStr));
+                numberStr = "";
+            }
+        }
+
+        if (line[i] == '|') {
+            winningNumbersSegment = false;
+            playerNumbersSegment = true;
+        }
+
+        if (playerNumbersSegment) {
+            if (isdigit(line[i])) {
+                numberStr += line[i];
+            } else if (numberStr != "") {
+                int checkNum = std::stoi(numberStr);
+                numberStr = "";
+                if(winningNumbers.find(checkNum) != winningNumbers.end()){
+                    nMatches += 1;
+                }
+            }
+        }
+
+        if (i == line.size() - 1 && numberStr != "") {
+            int checkNum = std::stoi(numberStr);
+            numberStr = "";
+            if(winningNumbers.find(checkNum) != winningNumbers.end()){
+                nMatches += 1;
+            }
+        }
+    }
+    return nMatches;
+}
+
 int sumScratchPoints(const std::string& file_name) {
 
     std::string line;
@@ -11,53 +61,54 @@ int sumScratchPoints(const std::string& file_name) {
     int sum = 0;
     if (f.is_open()) {
         while (getline(f, line)) {
-
-            std::string numberStr = "";
-            bool winningNumbersSegment = false;
-            bool playerNumbersSegment = false;
-            std::set <int> winningNumbers;
-            int nMatches = 0;
-            for (std::string::size_type i = 0; i < line.size(); i++) {
-
-                if (line[i] == ':') {
-                    winningNumbersSegment = true;
-                }
-
-                if (winningNumbersSegment) {
-                    if (isdigit(line[i])) {
-                        numberStr += line[i];
-                    } else if (numberStr != "") {
-                        winningNumbers.insert(std::stoi(numberStr));
-                        numberStr = "";
-                    }
-                }
-
-                if (line[i] == '|') {
-                    winningNumbersSegment = false;
-                    playerNumbersSegment = true;
-                }
-
-                if (playerNumbersSegment) {
-                    if (isdigit(line[i])) {
-                        numberStr += line[i];
-                    } else if (numberStr != "") {
-                        int checkNum = std::stoi(numberStr);
-                        numberStr = "";
-                        if(winningNumbers.find(checkNum) != winningNumbers.end()){
-                            nMatches += 1;
-                        }
-                    }
-                }
-
-                if (i == line.size() - 1 && numberStr != "") {
-                    int checkNum = std::stoi(numberStr);
-                    numberStr = "";
-                    if(winningNumbers.find(checkNum) != winningNumbers.end()){
-                        nMatches += 1;
-                    }
-                }
+            int nMatches = numMatches(line);
+            if (nMatches > 0) {
+                sum += pow(2, nMatches - 1);
             }
-            sum += pow(2, nMatches - 1);
+        }
+    } else {
+        perror("Error opening file");
+    }
+
+    return sum;
+}
+
+
+int sumNLinesRecursive(std::ifstream& f, int nLines) {
+
+    int sum = 0;
+    std::string line;
+    int nLinesComplete = 0;
+    while (getline(f, line) && ++nLinesComplete <= nLines) {
+        std::streampos fPos = f.tellg();
+
+        int nMatches = numMatches(line);
+        sum += 1 + sumNLinesRecursive(f, nMatches);
+
+        f.clear();
+        f.seekg(fPos);
+    }
+    return sum;
+}
+
+
+int sumScratchCards(const std::string& file_name) {
+
+    std::string line;
+    std::ifstream f (file_name);
+
+    int sum = 0;
+    if (f.is_open()) {
+        while (getline(f, line)) {
+
+            std::streampos fPos = f.tellg();
+
+            int nMatches = numMatches(line);
+            int recursiveMatches = sumNLinesRecursive(f, nMatches);
+            sum += 1 + recursiveMatches;
+
+            f.clear();
+            f.seekg(fPos);
         }
     } else {
         perror("Error opening file");
@@ -68,7 +119,9 @@ int sumScratchPoints(const std::string& file_name) {
 
 
 int main() {
-    int result = sumScratchPoints("data_real.txt");
-    std::cout << "Total of scratch points = " << result << std::endl;
+    int result_1 = sumScratchPoints("data_real.txt");
+    std::cout << "Total of scratch points = " << result_1 << std::endl;
+    int result_2 = sumScratchCards("data_real.txt");
+    std::cout << "Total of scratch cards = " << result_2 << std::endl;
     return 0;
 }
